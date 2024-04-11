e0ceee1439f0225baa6e9e7d3bdff2b8b6b8697d
EvilBobo <107318751+EvilBobo@users.noreply.github.com>
2024-04-11 18:56:28 +0100

    Back End Upload

---
+++ BE.js
@@ -0,0 +1,15 @@
+/* This is my Node.js file */
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send("Hi there");
+});
+
+app.get("/about", (request, response) => {
+    response.send("<h1>About</h1>");
+});
+
+app.listen(3000, () => {
+    console.log("Listen on the port 3000...");
+});
