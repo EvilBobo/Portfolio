d98819003b215e1b7814fd85e6a68f8886d51e7d
EvilBobo <107318751+EvilBobo@users.noreply.github.com>
2024-04-11 15:27:19 +0100

    Added my 3 sample files.

---
+++ FirstScript.js
@@ -0,0 +1,8 @@
+function myFunction()
+{
+    document.body.style.backgroundColor = "green";
+};
+
+
+function pink(){ document.body.style.backgroundColor = "pink"; }
+function sky(){ document.body.style.background = "skyblue"; }
